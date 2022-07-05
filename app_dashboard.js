68a0cf62a894583b20b1a167ab307fbd224baeb2
fabrizio <fabricimarelli@gmail.com>
2022-07-04 23:59:14 -0300

    dash_app_table

--- app_dashboard.js
+++ app_dashboard.js
@@ -1,8 +1,45 @@
-window.onload= async function(){
-    var cerrar_sesion=document.getElementById('cerrar-sesion')
+window.onload= function(){
+    
+    var cerrar_sesion=document.getElementById('cerrar-sesion');
+    var error_datos=document.getElementById('error-datos');
+    
+    
 
     cerrar_sesion.addEventListener("click", function(){
         localStorage.removeItem('login')
         window.location.href="index_login.html"
     })
-}
+
+    fetch("https://basic-server-one.vercel.app/users").then(
+        res=>{
+            res.json().then(
+                data=>{
+                    console.log(data.data);
+                    if(data.data.length > 0){
+                        error_datos.classList.add('ocultar-error')
+                        var temp = "";
+
+                        //lo recorre
+                        data.data.forEach((u)=>{
+                            temp += "<tr>";
+                            temp += "<td>"+u.id+"</td>";
+                            temp += "<td>"+u.name+"</td>";
+                            temp += "<td>"+u.email+"</td>";
+                            temp += "<td>"+u.phone+"</td>";
+                        });
+                        
+                        document.getElementById("data").innerHTML = temp;
+                    }
+                    
+                    if (res.status!=200) {
+                        console.log('hay un error');
+                        
+                        error_datos.classList.remove("ocultar-error");
+                    }
+                }
+            )
+        }
+    )
+
+}    
+
